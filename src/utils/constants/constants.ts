a8df61827fa44be720dda38517b990735508d4b1
elyoussfialaoui <abdellah.el-youssfi.alaoui@nemo.ma>
2024-02-18 20:09:25 +0100

    sub sectors and lazy loading for hscodes

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-     //baseUrl:'http://127.0.0.1:8883'
-     baseUrl:'http://54.174.23.211'
+     // baseUrl:'http://127.0.0.1:8883'
+     baseUrl:'http://47.242.127.40'
 
 }
